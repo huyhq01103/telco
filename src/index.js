dcf54d8292c4934e33a47a750c301ee1e37de40c
huyhq <huyhq@coccoc.vn>
2020-02-20 09:57:31 +0700

    add

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'normalize.css/normalize.css'
+import './styles/main.styl'
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOMContentLoaded', 'index')
+})
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+
+  // This option controls if and how source maps are generated.
+  // https://webpack.js.org/configuration/devtool/
+  devtool: 'eval-cheap-module-source-map',
+
+  // https://webpack.js.org/concepts/entry-points/#multi-page-application
+  entry: {
+    index: './src/index.js'
+  },
+  // output: {
+  //   path: path.resolve(__dirname, 'dist'),
+  //   filename: 'bundle.js',
+  //   publicPath: '/dist'
+  // },
+
+  // https://webpack.js.org/configuration/dev-server/
+  devServer: {
+    port: 8080,
+    writeToDisk: false // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
+  },
+
+  // https://webpack.js.org/concepts/loaders/
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      },
+      { test: /\.(styl|css)$/, loader: 'style-loader!css-loader!stylus-loader' },
+      {
+        // Load all images as base64 encoding if they are smaller than 8192 bytes
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // On development we want to see where the file is coming from, hence we preserve the [path]
+              name: '[path][name].[ext]?hash=[hash:20]',
+              esModule: false,
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  // https://webpack.js.org/concepts/plugins/
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'index.html'
+    })
+  ]
+}
